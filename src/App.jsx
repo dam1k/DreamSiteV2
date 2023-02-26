c7b57080c9a8e36e4f9509c8be4cdbf821030538
dam1k <tracacore123456789@gmail.com>
2023-02-26 14:20:23 +0200

    gle 43

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="container">
+      <h1>Hi mom</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import "./fonts/fonts.scss";
+import "./reset.scss";
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)

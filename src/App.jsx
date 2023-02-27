8505e0fb8ecaf62143976770e75f9ac2039baaf1
lxquids <lxquidsquad@gmail.com>
2023-02-27 21:36:11 +0200

    Header and Scrollbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { useStat, useEffect } from 'react'
 import './App.scss'
-import Navbar from './components/Navbar/Navbar'
 import Header from './components/Header/Header'
 
 
 function App() {
   return (
     <div className="App">
-      <div className="container">
-      <Navbar/>
-      <Header/>
+      <div className="wrapper">
+          <Header/>
       </div>
     </div>
   )
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
-// import ASCII from '../ASCII'
+import logo from "../../assets/logo.svg";
+import "./Header.scss";
 
 const Header = () => {
   return (
-    <div>
-     <h1>Leveraging the latest technologies to create cutting-edge websites that deliver results.</h1>
-     {/* <ASCII/> */}
-    </div>
+    <header className='header'>
+        <div className="header__container">
+            <img src={logo} alt="Logo"/>
+            <div className="header__right">
+            <nav className="nav">
+                <ul className="nav__links">
+                    <li><a href="#" className="nav__links">Projects</a></li>
+                    <li><a href="#" className="nav__links">About</a></li>
+                </ul>
+                <ul className="nav__links">
+                    <li><a href="#" className="nav__links">Instagram</a></li>
+                    <li><a href="#" className="nav__links">Facebook</a></li>
+                    <li><a href="#" className="nav__links">Facebook</a></li>
+                </ul>
+            </nav>
+                <div className="header__links">
+                    <a href="#" className="header__link">Telegram</a>
+                    <a href="#" className="header__link header__link--active">+373(22)71-09-40</a>
+                </div>
+            </div>
+        </div>
+    </header>
   )
 }
 

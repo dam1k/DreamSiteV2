db067754d1e74d94301654049b1a84b7e224e867
lxquids <lxquidsquad@gmail.com>
2023-02-28 14:59:47 +0200

    Links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,29 @@
 import React from 'react'
 import logo from "../../assets/logo.svg";
+import arrowupright from "../../assets/arrowupright.svg";
 import "./Header.scss";
 
 const Header = () => {
-  return (
+return (
     <header className='header'>
         <div className="header__container">
-            <img src={logo} alt="Logo"/>
+            <a href="#" className='header__logo'><img src={logo} alt="Logo"/></a>
             <div className="header__right">
-            <nav className="nav">
-                <ul className="nav__links">
-                    <li><a href="#" className="nav__links">Projects</a></li>
-                    <li><a href="#" className="nav__links">About</a></li>
-                </ul>
-                <ul className="nav__links">
-                    <li><a href="#" className="nav__links">Instagram</a></li>
-                    <li><a href="#" className="nav__links">Facebook</a></li>
-                    <li><a href="#" className="nav__links">Facebook</a></li>
-                </ul>
-            </nav>
+                <nav className="nav">
+                    <ul className="nav__links">
+                        <li>
+                            <a href="#" className="nav__link">
+                                Projects<i class="ri-arrow-right-up-line"></i>
+                            </a>
+                        </li>
+                        <li><a href="#" className="nav__link">About</a></li>
+                    </ul>
+                    <ul className="nav__links">
+                        <li><a href="#" className="nav__link">Instagram</a></li>
+                        <li><a href="#" className="nav__link">Facebook</a></li>
+                        <li><a href="#" className="nav__link">Behance</a></li>
+                    </ul>
+                </nav>
                 <div className="header__links">
                     <a href="#" className="header__link">Telegram</a>
                     <a href="#" className="header__link header__link--active">+373(22)71-09-40</a>
@@ -26,7 +31,7 @@ const Header = () => {
             </div>
         </div>
     </header>
-  )
+)
 }
 
 export default Header

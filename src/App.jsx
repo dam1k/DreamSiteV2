b6b332a875a00666736ef4bfc1bc473b44754f38
dam1k <tracacore123456789@gmail.com>
2023-03-04 19:44:14 +0200

    about

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from './components/Hero/Hero'
 // import Box from "./components/Three/Box";
 import Cylinder3d from './components/Three/Cylinder';
 import { Canvas } from "@react-three/fiber";
+import About from './components/About/About';
 
 function App() {
   return (
@@ -12,12 +13,8 @@ function App() {
       <div className="wrapper">
           <Header/>
           <Hero/>
-           <Canvas>
-          <pointLight position={[10, 10, 10]} />
-          <ambientLight /> 
-          <Cylinder3d position={[-1.5, 0, 0]} />
-          <Cylinder3d position={[1.2, 0, 0]} />
-        </Canvas>
+          <br></br>
+          <About/>
       </div>
     </div>
   )
